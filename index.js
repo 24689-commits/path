ff859913448b64b6ab74eeb2819e02e98759a883
Aphelele2 <aphelelejoyi@gmail.com>
2023-07-31 12:29:39 +0200

    done

---
+++ index.js
@@ -0,0 +1,8 @@
+const path = require('path')
+console.log(path.join(__dirname, 'index.js'));
+
+const path = require('path')
+console.log(__filename);
+
+const path = require('path')
+console.log(__dirname);
