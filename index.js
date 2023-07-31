62a0ee0d6ec46ad98ce30066bd711c072192f5e2
Aphelele2 <aphelelejoyi@gmail.com>
2023-07-31 13:37:05 +0200

    done

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//PATH
+
 const path = require('path')
 console.log(path.join(__dirname, 'index.js'));
 
@@ -6,3 +8,18 @@ console.log(__filename);
 
 const path = require('path')
 console.log(__dirname);
+
+//FILE SYSTEM
+
+const http = require('http')
+const port = ++process.env.port || 4000
+http.createServer((req, res)=>{
+    res.end('Well done')
+}).listen(port, () => {
+    console.log(`This is running on port ${port} `);
+})
+
+const http = require('http');
+http.createServer(function (req, res) {
+  return res.end('Hello, this is Aphelele Joyi');
+}).listen(8000);
